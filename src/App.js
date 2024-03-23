ae8911e49b8054074446f423ebf1fa9ad80717f0
Senanyo Benwari <benwarisenanyo@yahoo.com>
2024-03-23 12:46:24 +0000

    mod

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import { useState, useEffect } from "react";
 import './App.css';
+import axios from "axios";
 
 function App() {
+  const [data, setData] = useState("");
+  const [value, setValue] = useState(0);
+
+  const postData = async (data) => {
+    const {url, body} = data
+        return   axios
+                .post(url, body, 
+                    {
+                        headers:{"Content-Type": "application/json"}
+                    })
+            
+                .then( response =>  response)
+  }
+
+  const getData = async (url) => {
+        return   axios
+                .get(url, 
+                    {
+                        headers:{"Content-Type": "application/json"}
+                    })
+            
+                .then( response =>  response)
+  }
+
+  function handleChange(e){
+    e.preventDefault();
+    const target = e.target;
+    setData(target.value);
+  }
+
+  async function handleSubmit(e){
+    e.preventDefault();
+    const response = await postData({url: "http://127.0.0.1:8000/total/add/", body: data})
+    setValue(response.data)
+  }
+ 
+  async function initialValue(){
+    const response = await getData("http://127.0.0.1:8000/total/view/")
+    setValue(response.data)
+  }
+  useEffect(() => {
+    initialValue();
+  })
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Count away</p>
       </header>
+      <form onSubmit={handleSubmit}>
+      <input type='number' name='counter' placeholder='Type here...' onChange={handleChange} />
+      <button>Add</button>
+      <p>current total: {value}</p>
+      </form>
     </div>
   );
 }
